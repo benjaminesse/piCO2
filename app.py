9d87db7a12d692dd6d02af8eea020d2b9b8ad0c5
Ben Esse <ben_esse@outlook.com>
2025-01-20 16:19:00 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+import os
+import pandas as pd
+import plotly.express as px
+from flask import Flask
+from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input
+
+# Setup the Dash app
+server = Flask(__name__)
+app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.DARKLY])
+app.title = "PiChamber Dashboard"
+
+controls = dbc.Card(
+    [
+	
+	html.Div(
+            [
+                dbc.Button("Refresh", id="refresh", color="primary",
+                           style={"margin-left": "15px"})
+            ]
+        )
+    ]
+)
+
+# =============================================================================
+# App Plots
+# =============================================================================
+
+plots = dbc.Card(
+    [
+        html.Div(dcc.Graph(id="co2-chart")),
+	html.Div(dcc.Graph(id="temp-chart")),
+	html.Div(dcc.Graph(id="humid-chart"))
+    ],
+    body=True
+)
+
+app.layout = dbc.Container(
+    [
+        html.H1("PiChamber Dashboard"),
+        html.Div([dbc.Label("Status")], id="status-text"),
+        html.Hr(),
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(controls, md=4),
+                        dbc.Col(plots, md=8),
+                    ]
+                ),
+                dcc.Interval(
+                    id='interval-component',
+                    interval=5000,  # in milliseconds
+                    n_intervals=0
+                )
+            ]
+        )
+    ],
+    fluid=True
+)
+
+# =============================================================================
+# Callbacks
+# =============================================================================
+
+@app.callback(
+    [
+        Output("co2-chart", "figure"),
+        Output("temp-chart", "figure"),
+        Output("humid-chart", "figure"),
+        Output("status-text", "children")
+    ],
+    [
+        Input("refresh", "n_clicks"),
+        Input("interval-component", "n_intervals")
+    ]
+)
+def refresh(n, n_interval):
+    """Refresh app plot."""
+    # Get the results folder
+    res_fnames = os.listdir("Results")
+    res_fnames.sort()
+    fname = res_fnames[-1]
+
+    # Get the status
+    with open('status.txt', 'r') as r:
+        status = f'Status: {r.readline().strip()}'
+
+    # Read in the results
+    try:
+        df = pd.read_csv(f"Results/{fname}", parse_dates=True)
+        df = df.loc[-100:]
+    except FileNotFoundError:
+        # If the file is not found, return an empty DataFrame
+        cols = ["Time", "CO2 (ppm)", "Temperature (C)", "Humidity (%)"]
+        df = pd.DataFrame(columns=cols)
+
+    # Generate the time series figure
+    co2_fig = px.line(df, x="Time", y="CO2 (ppm)")
+    temp_fig = px.line(df, x="Time", y="Temperature (C)")
+    humid_fig = px.line(df, x="Time", y="Humidity (%)")
+    #time_fig.update_yaxes(range=limits)
+
+    return [co2_fig, temp_fig, humid_fig, status]
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host='0.0.0.0', port=4000)
---
+++ run_sensor.py
@@ -0,0 +1,68 @@
+import os
+import time
+import board
+import logging
+import adafruit_scd4x
+from datetime import datetime
+
+try:
+    with open('status.txt', 'w') as w:
+        w.write('Connecting')
+
+    # Get the start time
+    now_time = datetime.now()
+
+    # Set up logging
+    logger = logging.getLogger(__name__)
+
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(f"co2sensor.log", mode="a", encoding="utf-8")
+    formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s- %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    # Connect to the sensor
+    i2c = board.I2C()
+    scd4x = adafruit_scd4x.SCD4X(i2c)
+    logger.info(f"Serial number: {[hex(i) for i in scd4x.serial_number]}")
+    
+    scd4x.self_calibration_enabled = False
+    calib_flag = scd4x.self_calibration_enabled
+    logger.info(f'Calibration enabled: {calib_flag}')
+    
+    logger.info('Running self test')
+    print(scd4x.self_test())
+
+    # Initialise measurements
+    scd4x.start_periodic_measurement()
+    logger.info("Waiting for first measurement....")
+
+    # Ensure the results directory exists
+    if not os.path.isdir('Results'):
+        os.makedirs('Results')
+
+    # Create the output file
+    outfname = f'Results/{now_time}_co2.csv'
+    with open(outfname, 'w') as w:
+        w.write('Time,CO2 (ppm),Temperature (C),Humidity (%)\n')
+
+    # Set infinite loop to read data
+    while True:
+        if scd4x.data_ready:
+            with open('status.txt', 'w') as w:
+                w.write('Measuring')
+            with open(outfname, 'a') as w:
+                w.write(
+                    f'{datetime.now()},{scd4x.CO2},{scd4x.temperature}'
+                    f',{scd4x.relative_humidity}\n'
+                )
+            logger.info("Measurement recieved")
+        time.sleep(0.1)
+
+except Exception:
+    with open('status.txt', 'w') as w:
+        w.write('Error')
+    logger.error('Error encountered!', exc_info=True)
