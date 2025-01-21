1447b8883cb5a55da76ff238ccbd0b424851a37e
Ben <ben_esse@outlook.com>
2025-01-21 15:17:26 +0000

    Add GPS to the sensor

--- run_sensor.py
+++ run_sensor.py
@@ -4,25 +4,51 @@ import board
 import logging
 import adafruit_scd4x
 from datetime import datetime
+from gps import GPS
+from subprocess import call
 
 try:
-    with open('status.txt', 'w') as w:
-        w.write('Connecting')
-
-    # Get the start time
-    now_time = datetime.now()
 
     # Set up logging
     logger = logging.getLogger(__name__)
 
     logger.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(f"co2sensor.log", mode="a", encoding="utf-8")
-    formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s- %(message)s')
+    file_handler = logging.FileHandler(
+        f"co2sensor.log", mode="a", encoding="utf-8"
+    )
+    formatter = logging.Formatter(
+        '%(asctime)s-%(name)s-%(levelname)s: %(message)s'
+    )
     console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
+
+    # Try to connect to the GPS
+    with open('status.txt', 'w') as w:
+        w.write('Waiting for GPS...')
+
+    logger.info('Connecting to GPS')
+    # Connect to GPS
+    gps = GPS()
+
+    # Find the location and time
+    ts, lat, lon, alt, flag = gps.get_fix(time_to_wait=7200)
+    if flag:
+        logger.info(f'GPS fix found, updating system time to {ts}')
+        tstr = ts.strftime('%Y-%m-%d %H:%M:%S')
+        call(f'sudo date --set="{tstr}"', shell=True)
+        logger.info(f'Latitude = {lat}, Longitude = {lon}, Altitude = {alt} m')
+
+    else:
+        logger.warning('GPS fix failed, using Pi time', exc_info=True)
+
+    with open('status.txt', 'w') as w:
+        w.write('Connecting to sensor...')
+
+    # Get the start time
+    now_time = datetime.now()
 
     # Connect to the sensor
     i2c = board.I2C()
@@ -33,12 +59,9 @@ try:
     calib_flag = scd4x.self_calibration_enabled
     logger.info(f'Calibration enabled: {calib_flag}')
 
-    logger.info('Running self test')
-    print(scd4x.self_test())
-
     # Initialise measurements
     scd4x.start_periodic_measurement()
-    logger.info("Waiting for first measurement....")
+    logger.info("Waiting for first measurement...")
 
     # Ensure the results directory exists
     if not os.path.isdir('Results'):
@@ -47,6 +70,7 @@ try:
     # Create the output file
     outfname = f'Results/{now_time.strftime("%Y%m%dT%H%M%S")}_co2.csv'
     with open(outfname, 'w') as w:
+        w.write(f'Latitude,{lat}\nLongitude,{lon}\nAltitude,{alt} m\n')
         w.write('Time,CO2 (ppm),Temperature (C),Humidity (%)\n')
 
     # Set infinite loop to read data
